4f038142f4025bb151ca156c407293bd484d485f
Eetu Sutinen <eetu.sutinen@gmail.com>
2018-02-19 07:32:44 +0200

    use I18n component

--- src/components/__tests__/Dot.test.tsx
+++ src/components/__tests__/Dot.test.tsx
@@ -2,18 +2,23 @@ import * as React from 'react';
 import Dot from '../dot';
 import { actions } from '../../actions/dot';
 import { create } from 'react-test-renderer';
+import { I18nextProvider } from 'react-i18next';
 
-jest.mock('../../i18n', () => ({
-  default: {
-    t: (key: string) => key,
-  }
-}));
+import i18n from '../../i18nForTests';
+
+// jest.mock('../../i18n', () => ({
+//   default: {
+//     t: (key: string) => key,
+//   }
+// }));
 
 test('Dot should render', () => {
   const state = { dots: '' };
 
   const component = create(
-    <Dot actions={actions} state={state}/>
+    <I18nextProvider i18n={i18n}>
+      <Dot actions={actions} state={state} />
+    </I18nextProvider>
   );
 
   expect(component).toMatchSnapshot();
--- src/components/dot/index.tsx
+++ src/components/dot/index.tsx
@@ -2,24 +2,29 @@ import * as React from 'react';
 
 import { DotState } from '../../reducers/dot';
 import { actions } from '../../actions/dot';
-import i18n from '../../i18n';
 
 import './index.css';
+import { TranslationFunction } from 'i18next';
+import { I18n } from 'react-i18next';
 
 interface DotComponentProps {
   state: DotState;
   actions: typeof actions;
 }
 
-const Dot: React.StatelessComponent<DotComponentProps> = (
-  props: DotComponentProps,
-) => (
-  <div>
-    <div>
-      {i18n.t('Dots')}: <span className="Dot">{props.state.dots}</span>
-    </div>
-    <button onClick={props.actions.add}>{i18n.t('Add')}</button>
-  </div>
+const Dot: React.StatelessComponent<DotComponentProps> = (props: DotComponentProps) => (
+  <I18n ns="translations">
+  {
+    (t: TranslationFunction) => (
+      <div>
+        <div>
+          {t('Dots')}: <span className="Dot">{props.state.dots}</span>
+        </div>
+        <button onClick={props.actions.add}>{t('Add')}</button>
+      </div>
+    )
+  }
+  </I18n>
 );
 
 export default Dot;
